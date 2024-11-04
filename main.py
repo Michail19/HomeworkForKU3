50096d8802da55708313a74208c61c79ae13ce1e
Michail19 <Mi.ershov.2017@yandex.ru>
2024-11-04 21:52:01 +0300

    Added classes

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+import re
+import sys
+import yaml
+import argparse
+import math
 
+
+class ConfigParser:
+    def __init__(self, input_text):
+        self.input_text = input_text
+        self.constants = {}
+
+    def parse(self):
+        # Удаление многострочных комментариев
+        self.input_text = re.sub(r'#\|.*?\|#', '', self.input_text, flags=re.DOTALL)
+
+        # Разбор и обработка строк
+        lines = self.input_text.strip().splitlines()
+        config_data = self.parse_lines(lines)
+        return config_data
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Config to YAML converter")
+    parser.add_argument("output", help="Output file path")
+    args = parser.parse_args()
+
+    input_text = sys.stdin.read()
+    config_parser = ConfigParser(input_text)
+    config_data = config_parser.parse()
+
+    with open(args.output, 'w') as f:
+        yaml.dump(config_data, f)
+        
+
+if __name__ == "__main__":
+    main()
